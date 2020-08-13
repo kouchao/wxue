caa349a903b1f66e1b33c52d47bbc1c66707db86
kouchao <admin@jskou.com>
2020-08-14 00:23:35 +0800

    test: reactive测试用例

--- __tests__/reactive.spec.js
+++ __tests__/reactive.spec.js
@@ -1,19 +1,37 @@
-import { wxue, reactive } from '../src'
+import { wxue, reactive, nextTick } from '../src'
 describe('reactive', () => {
-  test('普通响应', () => {
+  test('普通响应', done => {
     wxue({
       setup () {
         const obj = reactive({ a: 0, b: 0 })
-        obj.a++
-        return { obj }
+        function add () {
+          obj.a++
+        }
+        return { obj, add }
+      },
+      async onLoad () {
+        this.add()
+        await nextTick()
+        expect(this.data.obj.a).toEqual(1)
+        done()
       }
     })
   })
-  // test('深度响应', () => {
-  //   wxue({
-  //     setup () {
-  //       const deepObj = reactive({ a: { b: { c: 0 } }, b: 0 })
-  //     }
-  //   })
-  // })
+  test('深度响应', done => {
+    wxue({
+      setup () {
+        const obj = reactive({ a: { b: { c: { d: 0 } } }, b: 0 })
+        function add () {
+          obj.a.b.c.d++
+        }
+        return { obj, add }
+      },
+      async onLoad () {
+        this.add()
+        await nextTick()
+        expect(this.data.obj.a.b.c.d).toEqual(1)
+        done()
+      }
+    })
+  })
 })
