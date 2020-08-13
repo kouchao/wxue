b6a1ce5dd43a69e295b18afd39d3553d40db5bfe
kouchao <admin@jskou.com>
2020-08-14 00:21:43 +0800

    test: 测试环境的setData

--- env/page.js
+++ env/page.js
@@ -9,7 +9,24 @@ class WxPage {
   }
 
   setData (data, fn) {
-    console.log('setData')
+    Object.keys(data).forEach(key => {
+      const keys = key.split('.')
+      if(keys.length === 1){
+        this.data[key] = data[key]
+      } else {
+        let end = this.data
+        keys.forEach((k, index) => {
+          if(!end[k]) {
+            end[k] = {}
+          }
+          if(index === keys.length - 1){
+            end[k] = data[key]
+          } else {
+            end = end[k]
+          }
+        }) 
+      }
+    })
     this.onRender()
     fn && fn()
   }
