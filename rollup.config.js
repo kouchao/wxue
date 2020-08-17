397039894f51b38417fca4c6fd4ed6ee0cdce1c0
kouchao <admin@jskou.com>
2020-08-17 15:27:35 +0800

    fix: process is not defined

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
 import { terser } from 'rollup-plugin-terser'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
+import inject from '@rollup/plugin-inject'
+import path from 'path'
 export default {
   input: 'src/index.js',
   output: [{
     file: 'dist/wxue.min.js',
     format: 'cjs'
   }],
-  plugins: [nodeResolve(), terser()]
+  plugins: [nodeResolve(), terser(), inject({
+    'process.env.NODE_ENV': path.resolve('env/process.js')
+  })]
 }
