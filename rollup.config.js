5457ba0c839d36feb27275d76a8bec9ac63efa61
kouchao <admin@jskou.com>
2020-08-17 14:10:25 +0800

    ci: rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import { terser } from 'rollup-plugin-terser'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 export default {
   input: 'src/index.js',
   output: [{
     file: 'dist/wxue.min.js',
-    format: 'cjs',
-    plugins: [terser()]
-  }]
+    format: 'cjs'
+  }],
+  plugins: [nodeResolve(), terser()]
 }
